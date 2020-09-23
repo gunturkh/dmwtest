918d8c1146c04d6a3e3d94b6527ebada30e9534c
Guntur <guntur@symboinsurance.com>
2020-09-23 22:47:29 +0700

    Set laravel and example template view structure

---
+++ test3/resources/views/layout.blade.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" >
+		<title>Startup 3.3 - Project</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+				<link rel="icon" href="i/favicon.png" type="image/x-icon">
+		<!-- Google Fonts -->
+				<link href="https://fonts.googleapis.com/css?family=Open+Sans:100,200,300,400,600,500,700,800,900|Open+Sans:100,200,300,400,500,600,700,800,900&amp;subset=latin" rel="stylesheet">
+				<!-- Bootstrap 4.3.1 CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<!-- Slick 1.8.1 jQuery plugin CSS (Sliders) -->
+		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+		<!-- Fancybox 3 jQuery plugin CSS (Open images and video in popup) -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+		<!-- AOS 2.3.4 jQuery plugin CSS (Animations) -->
+		<link href="https://unpkg.com/aos@2.3.4/dist/aos.css" rel="stylesheet">
+		<!-- FontAwesome CSS -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+		<!-- Startup 3 CSS (Styles for all blocks) -->
+					<link href="assets/css/style.css" rel="stylesheet" />
+				<!-- jQuery 3.3.1 -->
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+			</head> 
+	<body>
+<!-- Navigation Mobile type 2 -->
+
+<a class="open_menu color-main bg-light radius_full"><i class="fas fa-bars lh-40"></i></a>
+<div class="navigation_mobile bg-light type2">
+	<a class="close_menu color-main"><i class="fas fa-times"></i></a>
+	<div class="px-40 pt-60 pb-60 text-center inner">
+			
+		<div><a href="#" class="f-heading f-22 link color-main mb-20">Home</a></div>
+					
+		<div><a href="#" class="f-heading f-22 link color-main mb-20">Popular</a></div>
+					
+		<div><a href="#" class="f-heading f-22 link color-main mb-20">Recent</a></div>
+					
+		<div><a href="#" class="f-heading f-22 link color-main mb-20">Featured</a></div>
+					
+		<div><a href="#" class="f-heading f-22 link color-main mb-20">Designers</a></div>
+					
+		<div><a href="#" class="f-heading f-22 link color-main mb-20">Team</a></div>
+						<div><a href="#" class="f-12 link color-main op-7 text-uppercase sp-20 mb-20">Help</a></div>
+						<div><a href="#" class="f-12 link color-main op-7 text-uppercase sp-20 mb-20">F.A.Q.</a></div>
+						<div><a href="#" class="f-12 link color-main op-7 text-uppercase sp-20 mb-20">Support</a></div>
+						<div><a href="#" class="f-12 link color-main op-7 text-uppercase sp-20 mb-20">About Us</a></div>
+						<div><a href="#" class="f-12 link color-main op-7 text-uppercase sp-20 mb-20">Blog</a></div>
+						<div><a href="#" class="f-12 link color-main op-7 text-uppercase sp-20 mb-20">Careers</a></div>
+						<div class="socials mt-60">
+						<a href="#" target="_blank" class="link color-main f-18 mx-10"><i class="fab fa-twitter"></i></a>
+						<a href="#" target="_blank" class="link color-main f-18 mx-10"><i class="fab fa-facebook"></i></a>
+						<a href="#" target="_blank" class="link color-main f-18 mx-10"><i class="fab fa-dribbble"></i></a>
+						<a href="#" target="_blank" class="link color-main f-18 mx-10"><i class="fab fa-instagram"></i></a>
+						<a href="#" target="_blank" class="link color-main f-18 mx-10"><i class="fab fa-behance"></i></a>
+					</div>
+	</div>
+</div>	
+<!-- Header 6 -->
+
+<header class="pt-175 pb-110 bg-dark color-filter-dark-3 header_6">
+	<style>
+		.header_6{background-image:url(assets/i/header_6_bg.jpg);}
+		@media(-webkit-min-device-pixel-ratio: 1.25), (min-resolution: 120dpi){
+			.header_6{background-image:url(assets/i/header_6_bg@2x.jpg);}
+		}
+	</style>
+
+	<!-- Header Menu 6 -->
+
+	<nav class="header_menu_6 pt-30 pb-30 mt-30">
+		<div class="container mw-970 px-xl-0">
+			<div class="row justify-content-between align-items-center">
+				<div class="col-lg-5 d-flex align-items-center" data-aos-duration="600" data-aos="fade-down" data-aos-delay="1200">
+					<a href="#" class="link color-white f-18 medium mr-30">Products</a>
+					<a href="#" class="link color-white f-18 medium mr-30">Pricing</a>
+					<a href="#" class="link color-white f-18 medium">Community</a>
+				</div>
+				<div class="col-lg-2 text-center logo color-white" data-aos-duration="600" data-aos="fade-down" data-aos-delay="900">Startup 3</div>
+				<div class="col-lg-5 d-flex justify-content-end align-items-center" data-aos-duration="600" data-aos="fade-down" data-aos-delay="1200">
+					<a href="#" class="link color-white f-18 medium mr-30">Login</a>
+					<a href="#" class="btn sm action-1">Sign Up</a>
+				</div>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container px-xl-0 text-center">
+		<div class="mb-3 logo d-block d-xl-none color-white logo_mobile">Startup 3</div>
+		<div class="row justify-content-center">
+			<div class="col-lg-9" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
+				<h1 class="color-white">A high-quality solution for those who want a beautiful startup website quickly</h1>
+			</div>
+			<div class="col-lg-12 relative h-70 mt-80 mb-70 btn_holder" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
+				<a href="https://www.youtube.com/embed/RcTFVcoibI0?rel=0&showinfo=0" target="_blank" class="btn red play" data-fancybox="header_6_fancybox"><i class="fas fa-play"></i></a>
+			</div>
+			<div class="col-md-6 color-white f-18 medium text-adaptive" data-aos-duration="600" data-aos="fade-down" data-aos-delay="600">
+				We made it so beautiful and simple. It combines landings, 
+				pages, blogs and shop screens. It is definitely the tool you need 
+				in your collection!			</div>
+		</div>
+	</div>
+</header>
+
+<!-- Feature 40 -->
+
+<section class="pt-105 pb-95 bg-dark color-white feature_40">
+	<div class="container px-xl-0">
+		<div class="row justify-content-center">
+			<div class="col-xl-10">
+				<div class="row justify-content-between">
+					<div class="col-xl-7 col-md-6 mb-50 mb-md-0">
+						<h2 class="mb-20 small" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">Build Your Own Site <br />like a Big Company</h2>
+						<div class="f-22 text-adaptive" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
+							We have created a new product that will <br />
+							help designers, developers and companies <br />
+							create websites for their startups.						</div>
+						<span data-aos-duration="600" data-aos="fade-down" data-aos-delay="600"><a href="#" class="mt-35 btn action-1">Learn More</a></span>					</div>
+					<div class="col-md-5 col-sm-10 mt-15">
+						<div class="mb-35 block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="900">
+							<div class="mb-15 d-flex align-items-center f-22 title">
+								<i class="fas fa-cloud f-24 lh-32 w-30 mr-15"></i><div>Splashproof Design</div>
+							</div>
+							<div class="ml-45 text-adaptive">
+								You'll get a description of each component 
+								and learn how to connect and use them.							</div>
+						</div>
+						<div class="mb-35 block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="1200">
+							<div class="mb-15 d-flex align-items-center f-22 title">
+								<i class="fas fa-book f-24 lh-32 w-30 mr-15"></i><div>User Guide</div>
+							</div>
+							<div class="ml-45 text-adaptive">
+								At length one of them called out in a clear, 
+								polite, smooth dialect, not unlike in sound.							</div>
+						</div>
+					
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Feature 5 -->
+
+<section class="pt-105 pb-55 bg-light text-center feature_5">
+	<div class="container px-xl-0">
+		<div class="row justify-content-center">
+			<div class="col-xl-8 col-lg-10">
+				<h2 class="small" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">Create Something Awesome</h2>
+				<div class="mt-25 mb-45 f-22 color-heading text-adaptive description" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
+					We have created a new product that will help designers, developers 
+					and companies create websites for their startups quickly and easily.				</div>
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<div class="mb-50 col-xl-4 col-lg-5 col-md-6 col-sm-10 holder" data-aos-duration="600" data-aos="fade-down" data-aos-delay="600">
+				<img src="assets/i/feature_5_img_1.jpg" srcset="assets/i/feature_5_img_1@2x.jpg 2x" class="w-full radius10 noradius_bottom" alt="" />
+				<div class="px-45 pt-40 pb-45 radius10 noradius_top block">
+					<div class="mb-20 f-14 semibold text-uppercase sp-20 title">Convert Better</div>
+					<div class="color-heading text-adaptive">
+						Generate leads and sales with our 
+						effective call-to-action blocks 
+						from buttons to testimonials to 
+						forms.					</div>
+				</div>
+			</div>
+			<div class="mb-50 col-xl-4 col-lg-5 col-md-6 col-sm-10 holder" data-aos-duration="600" data-aos="fade-down" data-aos-delay="900">
+				<img src="assets/i/feature_5_img_2.jpg" srcset="assets/i/feature_5_img_2@2x.jpg 2x" class="w-full radius10 noradius_bottom" alt="" />
+				<div class="px-45 pt-40 pb-45 radius10 noradius_top block">
+					<div class="mb-20 f-14 semibold text-uppercase sp-20 title">Designed to Impress</div>
+					<div class="color-heading text-adaptive">
+						Carefully crafted precise design, 
+						with harmo-nious typography and 
+						perfect padding around every 
+						component.					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Form 14 -->
+
+<section class="pt-105 pb-100 bg-light form_14">
+	<div class="container px-xl-0">
+		<div class="row justify-content-center">
+			<div class="col-xl-8 col-lg-10">
+				<h2 class="small text-center" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">Create Billing Account</h2>
+				<form action="form/submit" method="post" class="mt-50 px-100 pt-85 pb-35 radius10">
+					<div class="row">
+						<div class="col-md-6 mb-35 block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
+							<div class="mb-10 f-14 semibold text-uppercase sp-20">First Name</div>
+							<input type="text" name="firstname" class="input w-full border-gray focus-action-1 color-heading placeholder-heading" required />
+						</div>
+						<div class="col-md-6 mb-35 block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
+							<div class="mb-10 f-14 semibold text-uppercase sp-20">Last Name</div>
+							<input type="text" name="lastname" class="input w-full border-gray focus-action-1 color-heading placeholder-heading" required />
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-8 mb-35 block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
+							<div class="mb-10 f-14 semibold text-uppercase sp-20">Address</div>
+							<input type="text" name="address" class="input w-full border-gray focus-action-1 color-heading placeholder-heading" required />
+						</div>
+						<div class="col-md-4 mb-35 block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
+							<div class="mb-10 f-14 semibold text-uppercase sp-20">Postal / Zip</div>
+							<input type="text" name="zip" class="input w-full border-gray focus-action-1 color-heading placeholder-heading" required />
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6 mb-35 block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
+							<div class="mb-10 f-14 semibold text-uppercase sp-20">City</div>
+							<input type="text" name="city" class="input w-full border-gray focus-action-1 color-heading placeholder-heading" required />
+						</div>
+						<div class="col-md-6 mb-35 block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
+							<div class="mb-10 f-14 semibold text-uppercase sp-20">Country</div>
+							<input type="text" name="country" class="input w-full border-gray focus-action-1 color-heading placeholder-heading" required />
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6 mb-35 block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
+							<div class="mb-10 f-14 semibold text-uppercase sp-20">Card Number</div>
+							<input type="text" name="card" class="js-card-mask input w-full border-gray focus-action-1 color-heading placeholder-heading" required />
+						</div>
+						<div class="col-md-4 col-sm-7 col-10 mb-35 block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
+							<div class="mb-10 f-14 semibold text-uppercase sp-20">Expiration</div>
+							<input type="text" name="exp" class="js-expiration-mask input w-full border-gray focus-action-1 color-heading placeholder-heading" required />
+						</div>
+						<div class="col-md-2 col-sm-5 col-6 mb-45 block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="600">
+							<div class="mb-10 f-14 semibold text-uppercase sp-20">CVV</div>
+							<input type="text" name="cvv" class="js-cvv-mask input w-full px-md-0 border-gray focus-action-1 color-heading placeholder-heading text-md-center" required />
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-6 col-md-8 mb-35 d-flex align-items-center block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
+							<i class="fab fa-paypal f-56 color-gray mr-25 flex-shrink-0"></i>
+							<div class="color-heading text-adaptive">Use your debit or credit <br />card to pay with PayPal</div>
+						</div>
+						<div class="col-lg-2 d-none d-lg-block"></div>
+						<div class="col-md-4 block text-md-right" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
+							<button class="btn action-1">Sign In</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Footer 2 -->
+
+<footer class="pt-70 pb-65 bg-light text-center footer_2">
+	<div class="container px-xl-0">
+		<div class="row justify-content-center">
+			<div class="col-12 lh-40" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
+				<a href="#" class="link color-main mx-20">Portfolio</a>
+				<a href="#" class="link color-main mx-20">How it Works</a>
+				<a href="#" class="link color-main mx-20">Pricing</a>
+				<a href="#" class="link color-main mx-20">About</a>
+				<a href="#" class="link color-main mx-20">Contacts</a>
+			</div>
+			<div class="mt-20 col-12" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
+				<div class="color-heading text-adaptive">
+					Be sure to take a look at our <a href="#" class="link color-heading">Terms of Use</a> 
+					and <a href="#" class="link color-heading">Privacy Policy</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+<!-- forms alerts -->
+<div class="alert alert-success alert-dismissible fixed-top alert-form-success" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	Thanks for your message!
+</div>
+<div class="alert alert-warning alert-dismissible fixed-top alert-form-check-fields" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	Please, fill in required fields.
+</div>
+<div class="alert alert-danger alert-dismissible fixed-top alert-form-error" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	<div class="message">An error occurred while sending data :( Please, check if your hosting supports PHP and check how to set form data sending <a href="https://designmodo.com/startup/documentation/#form-handler" target="_blank" class="link color-transparent-white">here</a>.</div>
+</div>
+
+<!-- gReCaptcha popup (uncomment if you need a recaptcha integration) -->
+<!--
+<div class="bg-dark op-8 grecaptcha-overlay"></div>
+<div class="bg-light radius10 w-350 h-120 px-20 pt-20 pb-20 grecaptcha-popup">
+	<div class="w-full h-full d-flex justify-content-center align-items-center">
+		<div id="g-recaptcha" data-sitekey="PUT_YOUR_SITE_KEY_HERE"></div>
+	</div>
+</div>
+<script src="https://www.google.com/recaptcha/api.js?render=explicit" async defer></script>
+-->
+
+<!-- Bootstrap 4.3.1 JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+<!-- Fancybox 3 jQuery plugin JS (Open images and video in popup) -->
+<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+<!-- Google maps JS API -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyDP6Ex5S03nvKZJZSvGXsEAi3X_tFkua4U"></script>
+<!-- Slick 1.8.1 jQuery plugin JS (Sliders) -->
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<!-- AOS 2.3.4 jQuery plugin JS (Animations) -->
+<script src="https://unpkg.com/aos@2.3.4/dist/aos.js"></script>
+<!-- Maskedinput jQuery plugin JS (Masks for input fields) -->
+<script src="assets/js/jquery.maskedinput.min.js"></script>
+<!-- Startup 3 JS (Custom js for all blocks) -->
+<script src="assets/js/script.js"></script>
+
+</body>
+</html>
